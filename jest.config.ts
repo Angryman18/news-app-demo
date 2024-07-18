14f66bcfef7bdec027d0fa4ee2d5a14ed6e3ee66
Shyam Mahanta <smahanta118@gmail.com>
2024-07-18 18:13:11 +0530

    added module directories

--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,13 @@ import type { Config } from "jest";
 export default async (): Promise<Config> => {
   return {
     testEnvironment: "jsdom",
-    moduleFileExtensions: ["js", "ts", "jsx", "tsx", "css"],
+    moduleFileExtensions: ["js", "ts", "jsx", "tsx"],
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
     preset: "ts-jest",
     transform: {
       "\\.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.app.json" }],
     },
+    moduleDirectories: ["<rootDir>", "node_modules"],
     verbose: true,
     moduleNameMapper: {
       "\\.(css|less|sass|scss)$": "<rootDir>/__mock__/CssStub.ts",
